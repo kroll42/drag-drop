c1e76a9e36c3eebd31e67a6ee8ece10781908030
karolyne muniz <munizkarolyne42@gmail.com>
2024-08-06 13:11:32 -0400

    Add remaining project files

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,76 @@
+
+import {useRef, useEffect, useState} from "react";
+import Trash from "../icons/Trash";
+
+const NoteCard = ({ note }) => {
+    const body = JSON.parse(note.body);
+    const [position, setPosition] = useState(JSON.parse(note.position));
+    const colors = JSON.parse(note.colors);
+
+    let mouseStartPos = {x: 0, y: 0};
+    const cardRef = useRef(null);
+
+    useEffect(() => {
+        autoGrow(textAreaRef)
+    }, [])
+    const textAreaRef = useRef(null);
+    const autoGrow = (textAreaRef) => {
+        const {current} = textAreaRef;
+        current.style.height = "auto"; // Reset the height
+        current.style.height = current.scrollHeight + "px"; // Set the new height
+    }
+
+    const mouseDown = (e) => {
+        mouseStartPos.x = e.clientX
+        mouseStartPos.y = e.clientY
+
+        document.addEventListener('mousemove', mouseMove)
+    }
+
+    const mouseMove = (e) => {
+        const mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        setPosition({
+            x:cardRef.current.offsetLeft - mouseMoveDir.x,
+            y:cardRef.current.offsetTop - mouseMoveDir.y,
+        })
+    };
+
+    return (
+        <div
+            ref={cardRef}
+            className="card" 
+            style={{ backgroundColor: colors.colorBody ,
+                left:`${position.x}px`, 
+                top:`${position.y}px`,
+            }}>
+            
+            <div
+                onMouseDown={mouseDown}
+                className="card-header" 
+                style={{backgroundColor: colors.colorHeader }}
+                
+            >
+                <Trash/>
+            </div>
+
+            <div className="card-body">
+                 <textarea
+                    ref={textAreaRef}
+                   style={{ color: colors.colorText }}
+                   defaultValue={body}
+                   onInput={ () => {autoGrow(textAreaRef)}}
+                 ></textarea>
+            </div>
+
+        </div>
+    );
+};
+
+export default NoteCard;
